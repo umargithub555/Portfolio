742fc0d8db20399080a1b9cb882d4350d014ca70
Shayan Umar <shayanumar5555@gmail.com>
2026-07-28 17:18:12 +0500

    added required svg icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,68 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+// import motion from './motion.svg'
+import mui from './mui.svg'
+import nextjs from './nextjs.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import car from './car.svg'
+import estate from './estate.svg'
+import pricewise from './pricewise.svg'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.svg'
+import threads from './threads.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import python from './generated/python.svg'
+import pytorch from './generated/pytorch.svg'
+import tensorflow from './generated/tensorflow.svg'
+import aws from './generated/aws.svg'
+import docker from './generated/docker.svg'
+import postgresql from './generated/postgresql.svg'
+import huggingface from './generated/huggingface.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+
+export {
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    mongodb,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    car,
+    estate,
+    pricewise,
+    snapgram,
+    summiz,
+    threads,
+    arrow,
+    contact,
+    python,
+    pytorch,
+    tensorflow,
+    aws,
+    docker,
+    postgresql,
+    soundon,
+    soundoff,
+    huggingface
+}
